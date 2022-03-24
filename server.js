c3de9b135cd8c1521b7095741c20b15137da8695
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-23 20:14:56 -0500

    saved a copy of server.js file (starter code) as server_ORIGINAL.js and recreated file initializing handlebars.js, adding session cookies, middleware and updated ports

--- server.js
+++ server.js
@@ -1,70 +1,43 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ =require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
+const path = require('path');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const sequelize = require('./config/connection');
 
-let posts=[];
-app.get("/",function(req,res){
-  res.render("home",{
-    startingContent:homeStartingContent,
-    posts:posts
+// set up handlebars.js
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+
+// session cookies
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({ db: sequelize }),
+};
+
+// insert middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// session middleware
+app.use(session(sess));
+
+// routes
+app.use(require('./controllers/index'));
+
+const PORT = process.env.PORT || 3001;
+
+// form connection to sequelize database BEFORE starting server.
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}!`);
   });
-
-});
-
-app.get("/contact",function(req,res){
-  res.render("contact",{contactContent:contactContent});
-});
-app.get("/about",function(req,res){
-  res.render("about",{aboutContent:aboutContent});
-});
-
-
-app.get("/compose",function(req,res){
-  res.render("compose");
-});
-
-app.post("/compose",function(req,res){
-  const post={
-    title:req.body.postTitle,
-    content:req.body.postBody
-  };
-posts.push(post);
-res.redirect("/");
-});
-
-
-
-
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle=_.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){
-    const storedTitle=_.lowerCase(post.title);
-
-    if(storedTitle===requestedTitle){
-      res.render("post",{
-        title:post.title,
-        content: post.content
-      });
-    }
-  });
-
-});
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
 });
