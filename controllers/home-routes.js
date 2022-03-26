fb6d119b85e93aec9c8698dcbcc7ff0466f8694d
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-26 15:27:43 -0500

    changed sorting order to latest post on top on home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,7 @@ const { Post, User, Comment } = require('../models');
 router.get('/', (req, res) => {
   // console.log(req.session);
   Post.findAll({
+    order: [['created_at', 'DESC']],
     attributes: ['id', 'title', 'created_at', 'post_content'],
     include: [
       {
