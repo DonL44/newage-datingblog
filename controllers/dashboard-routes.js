74717fac1aa1a492234ea3ac524f24ab1c4b9534
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-27 06:18:24 -0500

    changed sorting in dashboard-routes.js so newest post shows on top

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,6 +9,7 @@ router.get('/', withAuth, (req, res) => {
       // use the ID from the session
       user_id: req.session.user_id,
     },
+    order: [['created_at', 'DESC']],
     attributes: ['id', 'title', 'created_at', 'post_content'],
     include: [
       {
