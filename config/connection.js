020a6cd9485b72a18286e4e9a79122bc0d14021b
Donovan <Donovan1478@yahoo.com>
2022-04-03 12:37:27 -0500

    updated code

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 // Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
