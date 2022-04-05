f3cc19f9a394733b677142eec95898e75ac2c2d5
Donovan <Donovan1478@yahoo.com>
2022-04-05 17:18:12 -0500

    removed twitter

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -17,12 +17,12 @@ router.get('/', withAuth, (req, res) => {
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['username', 'twitter'],
+          attributes: ['username'],
         },
       },
       {
         model: User,
-        attributes: ['username', 'twitter'],
+        attributes: ['username'],
       },
     ],
   })
@@ -49,12 +49,12 @@ router.get('/edit/:id', withAuth, (req, res) => {
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['username', 'twitter'],
+          attributes: ['username',
         },
       },
       {
         model: User,
-        attributes: ['username', 'twitter'],
+        attributes: ['username'],
       },
     ],
   })
@@ -91,12 +91,12 @@ router.get('/create/', withAuth, (req, res) => {
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['username', 'twitter'],
+          attributes: ['username'],
         },
       },
       {
         model: User,
-        attributes: ['username', 'twitter'],
+        attributes: ['username'],
       },
     ],
   })
